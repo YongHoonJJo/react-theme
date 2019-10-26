dcef769e7b3ae379523481256d53c436dcc28953
YongHoonJJo <starctak@gmail.com>
2019-10-27 02:38:39 +0900

    [Update] update custom style

--- src/styles/Select/index.js
+++ src/styles/Select/index.js
@@ -19,4 +19,45 @@ export default SelectComponent
 
 const CustomSelect = styled.div`
   position: relative;
+  border: 1px solid #1cbe9d;
+  border-radius: 5px / 5px;
+  width: 250px;
+  height: 35px;
+  cursor: pointer;
+
+  & select {
+    width: 100%;
+    height: 100%;
+    background: none;
+    border: none;
+  }
+
+  & select:focus {
+    outline: none;
+  }
+
+  &:before {
+    content: "";
+    background: #1cbe9d;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 40px;
+    pointer-events: none;
+    border-radius: 0 3px 3px 0 / 0 3px 3px 0;
+  }
+
+  &:after {
+    content: "";
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 10px 7px 0;
+    border-color: #fff transparent transparent transparent;
+    position: absolute;
+    right: 11px;
+    top: 50%;
+    transform: translateY(-50%);
+  }  
 `
--- src/views/Select.js
+++ src/views/Select.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 
 import Layout from '../layout/DefaultLayout'
 import Select, { Option } from '../styles/Select'
 
 const SelectView = () => {
-  const [state, setState] = useState({
+  const [state] = useState({
     options: [{
       value: 'Option1',
       label: 'Option1'
@@ -26,17 +27,46 @@ const SelectView = () => {
   )
   
 
-  const { value, options } = state
+  const { options } = state
 
   return (
     <Layout>
-      <Select>
-        {options.map(opt => {
-          return <Option key={opt.value} label={opt.label} value={opt.value} />
-        })}
-      </Select>
+      <Wrap>
+        <h1>Select</h1>
+        <p>When there are plenty of options, use a drop-down menu to display and select desired ones.</p>
+      </Wrap>
+      <Wrap>
+        <h2>Basic Usage</h2>
+        <Select>
+          {options.map(opt => {
+            return <Option key={opt.value} label={opt.label} value={opt.value} />
+          })}
+        </Select>
+      </Wrap>
     </Layout>
   )
 }
 
 export default SelectView
+
+const Wrap = styled.div`
+  padding-bottom: 20px;
+
+  & h1 {
+    font-size: 30px;
+    font-weight: 400;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  & h2 {
+    font-size: 24px;
+    font-weight: 350;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  & p {
+    color: #7F8A9A;
+  }
+`
